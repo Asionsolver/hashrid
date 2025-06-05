81efa18042628aaa51bbc1dd6cc4612b15d220b6
Asionsolver <asionsolver274@gmail.com>
2025-06-06 00:44:12 +0600

    add new section name like FAQs section in Hashrid project

--- script.js
+++ script.js
@@ -1,47 +1,15 @@
-// const canvas = document.getElementById("neonCanvas");
-// const ctx = canvas.getContext("2d");
-// canvas.width = window.innerWidth;
-// canvas.height = window.innerHeight;
+const faqItems = document.querySelectorAll(".faq-item");
 
-// function resizeCanvas() {
-//   canvas.width = window.innerWidth;
-//   canvas.height = window.innerHeight;
-// }
-// window.addEventListener("resize", resizeCanvas);
+faqItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    const isActive = item.classList.contains("active");
 
-// // Get CSS variable value from root
-// function getCSSVariable(name) {
-//   return getComputedStyle(document.documentElement)
-//     .getPropertyValue(name)
-//     .trim();
-// }
+    // Close all
+    faqItems.forEach((i) => i.classList.remove("active"));
 
-// const neonColor = getCSSVariable("--color-neon");
-// const blur = parseInt(getCSSVariable("--shadow-blur")) || 15;
-// const lineWidth = parseInt(getCSSVariable("--line-width")) || 3;
-
-// function drawLinePath(path, color = neonColor) {
-//   ctx.beginPath();
-//   ctx.moveTo(...path[0]);
-//   for (let i = 1; i < path.length; i++) {
-//     ctx.lineTo(...path[i]);
-//   }
-
-//   ctx.strokeStyle = color;
-//   ctx.shadowColor = color;
-//   ctx.shadowBlur = blur;
-//   ctx.lineWidth = lineWidth;
-//   ctx.stroke();
-// }
-// ctx.clearRect(0, 0, canvas.width, canvas.height);
-// drawLinePath([
-//   [-0, canvas.height - 100],
-//   [200, canvas.height - 100],
-//   [200, canvas.height - 300],
-//   [400, canvas.height - 300],
-// ]);
-// drawLinePath([
-//   [canvas.width - 300, 200],
-//   [canvas.width - 300, 400],
-//   [canvas.width - 100, 400],
-// ]);
+    // Toggle current
+    if (!isActive) {
+      item.classList.add("active");
+    }
+  });
+});
